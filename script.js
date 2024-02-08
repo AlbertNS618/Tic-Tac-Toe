e6fa0b6e9b360c03c57c8f3f7c2c957d0d3ee455
AlbertNS <albertnathan.2003@gmail.com>
2024-02-08 23:34:57 +0700

    build: use jquery for js

--- script.js
+++ script.js
@@ -1,32 +1,35 @@
-var play = 0;
-const boxBoard = document.querySelectorAll(".board div");
+var play = 0, game = 0;
 var x = new Array(), y = new Array();
 
-for(var i = 0; i < 9 ; i++){
-    boxBoard[i].addEventListener("click", function(){
-        if(this.innerHTML == ""){
+$(".board div").click(function () { 
+    if(game == 0){
+        if($(this).html() == ""){
             if(play == 0 || play % 2 == 0){
-                x.push(this.getAttribute("data-spot"));
-                this.innerHTML = "X";
-                play+=1;
-            }else{
-                y.push(this.getAttribute("data-spot"));
-                this.innerHTML = "O";
-                play+=1;
+                x.push($(this).attr("data-spot"));
+                $(this).html("X");
+                play += 1;
+            }
+            else{
+                y.push($(this).attr("data-spot"));
+                $(this).html("O");
+                play += 1;
             }
         }
-        decision();  
-    })
-}
+    }
+    decision();
+});
+
 
 function decision(){
+    console.log(play);
     if(x.includes("1") && x.includes("2") && x.includes("3") || x.includes("1") && x.includes("3") && x.includes("7")
     || x.includes("4") && x.includes("5") && x.includes("6") || x.includes("2") && x.includes("5") && x.includes("8")
     || x.includes("7") && x.includes("8") && x.includes("9") || x.includes("3") && x.includes("6") && x.includes("9")
     || x.includes("1") && x.includes("5") && x.includes("9")
     || x.includes("3") && x.includes("5") && x.includes("7")
     ){
-        document.querySelector(".game").innerHTML = "X WIN!"
+        $(".game").html("X WIN!");
+        game = 1;
         // for(var i = 0; i < 9 ; i++){
         //     boxBoard[i].replaceWith(boxBoard[i]);
         // }    
@@ -38,20 +41,25 @@ function decision(){
     || y.includes("1") && y.includes("5") && y.includes("9")
     || y.includes("3") && y.includes("5") && y.includes("7")
     ){
-        document.querySelector(".game").innerHTML = "O WIN!"
+        $(".game").html("O WIN!");
+        game = 1;
         // for(var i = 0; i < 9 ; i++){
         //     boxBoard[i].replaceWith(boxBoard[i].cloneNode(true));
         // }    
     }
+
+    if(play == 9 && game == 0){
+        $(".game").html("DRAW!");
+    }
 }
 
-document.querySelector('.refresh-button').addEventListener("click", function(){
+$(".refresh-button").click(function() {
+    $(".board div").html("");
     for(var i = 0; i < 9 ; i++){
-        boxBoard[i].innerHTML = "";
         x.pop();
         y.pop();    
     }
-    document.querySelector(".game").innerHTML = ""
+    $(".game").html("");
     play = 0;
+    game = 0;
 });
-
